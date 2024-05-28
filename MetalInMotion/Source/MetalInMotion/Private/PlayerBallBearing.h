e4daa5ee151ad2bdfa1554043c65b930ad1eec06
Gregory Allen <traquanallen.gtta@gmail.com>
2024-05-28 14:54:59 -0400

    init project
    getting setup with downloading unreal from source and making sure everything runs finely before i begin project

---
+++ MetalInMotion/Source/MetalInMotion/Private/PlayerBallBearing.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BallBearing.h"
+#include "PlayerBallBearing.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APlayerBallBearing : public ABallBearing
+{
+	GENERATED_BODY()
+};
